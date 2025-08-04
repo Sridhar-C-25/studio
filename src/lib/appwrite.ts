20aa7a9f9cd5f0750b3dd2ca6a1b289ac889d176
Sridhar Chandrasekar <iamsmartsri@gmail.com>
2025-08-04 15:23:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,8 +3,13 @@ import { Client, Databases, Account } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
-    .setProject(process.env.APPWRITE_PROJECT_ID!);
+    .setEndpoint(process.env.APPWRITE_ENDPOINT || '')
+    .setProject(process.env.APPWRITE_PROJECT_ID || '');
+
+if (process.env.APPWRITE_API_KEY) {
+  client.setKey(process.env.APPWRITE_API_KEY);
+}
+
 
 export const account = new Account(client);
 export const databases = new Databases(client);
