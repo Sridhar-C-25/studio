5a63e1c50037b800ccfb23daaaaba703422d4401
Sridhar Chandrasekar <iamsmartsri@gmail.com>
2025-08-06 17:34:58 +0000

    import { redirect } from 'next/navigation'
    export default function Page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,8 +1,6 @@
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Hello</h1>
-    </main>
-  );
+import { redirect } from 'next/navigation'
+
+export default function Page() {
+  redirect('/blogs')
 }
