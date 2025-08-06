8eac543504145c323a56122189cfb29982affa84
Sridhar Chandrasekar <iamsmartsri@gmail.com>
2025-08-06 17:00:04 +0000

    now i want create one homepage that will for end user. create one simple

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/dashboard');
-  return null;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Hello</h1>
+    </main>
+  );
 }
