a37d5eef0be6ec59d11dc79d9ca7dea71831925a
sridhar <75136330+Sridhar-C-25@users.noreply.github.com>
2025-08-23 09:17:30 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,23 @@
+import { MetadataRoute } from "next";
 
-import { MetadataRoute } from 'next'
- 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/dashboard/',
+      userAgent: "*",
+      allow: [
+        "/",
+        "/about",
+        "/contact",
+        "/blogs",
+        "/blogs/*",
+        "/blogs/category/*",
+        "/youtube",
+        "/privacy-policy",
+        "/terms-conditions",
+      ],
+      disallow: "/dashboard/",
     },
     sitemap: `${baseUrl}/sitemap.xml`,
-  }
+  };
 }
